43c9d697867ce25beae3f9aaa3f4bc4a40ed18c5
Jose Manuel Cavallini Malaver <jcavallinimalaver@frba.utn.edu.ar>
2023-04-23 21:45:19 -0300

    c

--- 00-CHelloWorld/hello.c
+++ 00-CHelloWorld/hello.c
@@ -4,5 +4,7 @@
 int main() {
    // printf() displays the string inside quotation
    printf("Hello, World!");
+   FILE *Flujo = fopen ( " ouput.txt " , " wb " );
+   fprintf (Flujo, " Hola mundo. fprintf " );
    return 0;
 }
